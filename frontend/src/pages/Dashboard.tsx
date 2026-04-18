27079460b0359a437eab70994bd7202be694fd1d
Amar Kumaraiah <amarkumaraiah@Amars-MacBook-Air.local>
2026-04-17 19:06:53 -0700

    Add root package.json for Vercel build

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useRecommendations } from '../hooks/useRecommendations'
 import RecommendationCard from '../components/RecommendationCard'
